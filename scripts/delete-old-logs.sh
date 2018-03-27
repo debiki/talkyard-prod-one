e67f8a507dbfb60e03ee5e792ac8c0fe561c89d3
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2018-03-27 11:11:32 +0200

    Rotate weekly not hourly.

--- scripts/delete-old-logs.sh
+++ scripts/delete-old-logs.sh
@@ -8,8 +8,8 @@ deleted_logs=./deleted-logs.tmp.log
 truncate -s0 $deleted_logs
 
 # Delete logs older than x days.
-find /var/log/postgresql/    -type f -name '*.log' -mtime +90 -print -delete >> $deleted_logs
-find /var/log/redis/         -type f -name '*.log' -mtime +90 -print -delete >> $deleted_logs
+find /var/log/postgresql/    -type f -name '*.log' -mtime +89 -print -delete >> $deleted_logs
+find /var/log/redis/         -type f -name '*.log' -mtime +89 -print -delete >> $deleted_logs
 
 log_message "Deleted these logs: `cat $deleted_logs`"
 
